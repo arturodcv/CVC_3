374b34bd59e69f8f59ce21b0625b74069899aa20
arturodcv <59840729+arturodcv@users.noreply.github.com>
2022-02-14 00:04:47 +0100

    Update nest_values.py

--- nest_values.py
+++ nest_values.py
@@ -6,7 +6,7 @@ results_path = 'results_folder'
 positions_path = 'positions_folder'
 input_images_path = 'input_images_folder' 
 gabor_folder = 'gabor_outputs'
-collect_data_folder = 'results_collect_data_36'
+collect_data_folder = 'results_collect_data_40'
 
 
 #Simulation 
@@ -100,9 +100,9 @@ rescale = 2.0 ; radius_lat = 2.5
 # Short lateral connections
 ratio_inh_exc_w = 4.0
 p_center_inh_exc = 1.0 ;        weight_inh_exc = - ratio_inh_exc_w * 0.2
-p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.1
+p_center_inh_inh = 1.0 ;        weight_inh_inh = - ratio_inh_exc_w * 0.05
 p_center_exc_exc = 1.0 ;        weight_exc_exc =  0.6
-p_center_exc_inh = 1.0 ;        weight_exc_inh =  0.8
+p_center_exc_inh = 1.0 ;        weight_exc_inh =  1.0
 
 input_weight_poiss_exc = 1.0     
 input_weight_poiss_inh = 0.25
